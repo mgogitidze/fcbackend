c89dbe76ab768618a49f186646885753256858af
Maksim Gogitidze <maksim.gogitidze@rt.ru>
2024-10-19 00:38:24 +0300

    init commit

---
+++ src/test/kotlin/com/example/fcbackend/FcbackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.fcbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FcbackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
